0c46b840757e5fa11c23bb1971b1d81e685cbdab
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-04 12:01:51 +0700

    create exam_results table

---
+++ database/migrations/2025_03_04_045827_create_exam_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exam_results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('exam_id');
+            $table->unsignedBigInteger('student_id');
+            $table->integer('score');
+            $table->timestamps();
+
+            $table->foreign('exam_id')->references('id')->on('exams')->onUpdate('restrict')->onDelete('cascade');
+        $table->foreign('student_id')->references('id')->on('users')->onUpdate('restrict')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exam_results');
+    }
+};
