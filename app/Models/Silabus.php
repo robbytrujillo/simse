70fed55ad57fe6904d7145bb5c104b73636a56d9
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-06 03:50:50 +0700

    add Model Silabus Type

--- app/Models/Silabus.php
+++ app/Models/Silabus.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Silabus extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['curriculum_id', 'class_id', 'content', 'mapel_id'];
+
+    public function curriculum()
+    {
+        return $this->belongsTo(Kurikulum::class, 'curriculum_id');
+    }
+    public function classRoom()
+    {
+        return $this->belongsTo(ClassRoom::class, 'class_id');
+    }
+    public function mapel()
+    {
+        return $this->belongsTo(Mapel::class, 'mapel_id');
+    }
 }
+
