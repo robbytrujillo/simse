60a0fe306003758a857bec1bb4882690488ecc15
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-05 22:56:51 +0700

    add Model Sanction Type

--- app/Models/SanctionType.php
+++ app/Models/SanctionType.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SanctionType extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function violations() {
+        return $this->hasMany(Violation::class, 'sanction_type_id');
+    }
 }
