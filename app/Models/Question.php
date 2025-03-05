e86cda6febf4411c465768f07ac87b2903f9c088
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-05 21:26:17 +0700

    add Model Question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'exam_id',
+        'question_text',
+        'question_type',
+    ];
+
+    public function exam() {
+        return $this->belongsTo(Exam::class, 'exam_id');
+    }
+
+    public function options() {
+        return $this->hasMany(Option::class, 'question_id');
+    }
+
+    public function studentAnswers() {
+        return $this->hasMany(StudentAnswer::class, 'question_id');
+    }
 }
