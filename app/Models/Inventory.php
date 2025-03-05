c6a43eff3346aa51b512c289916fb43d3279ad3a
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-05 13:43:49 +0700

    inventory model

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -4,7 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Inventory extends Model
 {
-    //
+    protected $fillable = [
+        'item_code',
+        'item_name',
+        'category_id',
+        'vendor_id',
+        'description',
+        'class_id',
+        'quantity',
+        'condition',
+        'procurement_date',
+        'remarks',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function classroom()
+    {
+        return $this->belongsTo(ClassRoom::class, 'class_id');
+    }
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
 }
+
