368285aeb6898a59cfcfc5cef8369778653332de
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-06 04:37:27 +0700

    add Violation Type

--- app/Models/ViolationType.php
+++ app/Models/ViolationType.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ViolationType extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function violations() {
+        return $this->hasMany(Violation::class, 'violation_type_id');
+    }
 }
