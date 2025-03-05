a6c943d5858d1951f23cecb726467a90bacfa7c2
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-06 04:05:47 +0700

    add Model Teacher

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -2,9 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'full_name',
+        'birth_place',
+        'birth_date',
+        'gender',
+        'address',
+        'phone',
+        'email',
+        'last_education',
+        'education_institution',
+        'graduation_year',
+        'employee_id',
+        'employment_status',
+        'position',
+        'start_date',
+        'user_id',
+        'image',
+    ];
+    public function classrooms()
+    {
+        return $this->hasMany(ClassRoom::class);
+    }
+    public function teachingData()
+    {
+        return $this->hasMany(TeachingData::class, 'teacher_id');
+    }
+    public function exams()
+    {
+        return $this->hasMany(Exam::class, 'teacher_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
