a2634f46a36fde45f143f55941e2fa8fc5408dfd
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-06 04:06:58 +0700

    add Model Teaching Data

--- app/Models/TeachingData.php
+++ app/Models/TeachingData.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TeachingData extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'teaching_data';
+
+    protected $fillable = [
+        'teacher_id',
+        'class_id',
+        'mapel_id',
+        'hours_per_week',
+        'day',
+        'start_time',
+        'end_time',
+    ];
+    protected $casts = [
+        'day' => 'array',
+        'start_time' => 'array',
+        'end_time' => 'array',
+    ];
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class, 'teacher_id');
+    }
+    public function mapel()
+    {
+        return $this->belongsTo(Mapel::class, 'mapel_id');
+    }
+    public function classRoom()
+    {
+        return $this->belongsTo(ClassRoom::class, 'class_id');
+    }
 }
+
