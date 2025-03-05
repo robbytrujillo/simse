17a1f46459256c0e6ff6bf96e655086c60de3ed7
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-05 13:48:14 +0700

    add Model Mapel

--- app/Models/Mapel.php
+++ app/Models/Mapel.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Mapel extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'nama_mapel',
+    ];
+    public function teachingData()
+    {
+        return $this->hasMany(TeachingData::class, 'mapel_id');
+    }
+    public function silabus()
+    {
+        return $this->hasMany(Silabus::class, 'mapel_id');
+    }
+    public function exams()
+    {
+        return $this->hasMany(Exam::class, 'mapel_id');
+    }
 }
+
