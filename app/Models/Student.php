f26ef75a76b745bd5a2c23730f32c4f2cf215aa1
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-06 03:58:20 +0700

    add Model Student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nis',
+        'name',
+        'gender',
+        'dob',
+        'address',
+        'phone',
+        'class_id',
+        'father_name',
+        'slug',
+        'image',
+        'email',
+        'user_id',
+    ];
+
+    public function classRoom()
+    {
+        return $this->belongsTo(ClassRoom::class, 'class_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
