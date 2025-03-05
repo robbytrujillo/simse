56fdcc14133925e459095b7bfcc338c878ce8ff7
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-06 04:21:51 +0700

    add Model Vendor

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Vendor extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function inventories() {
+        return $this->hasMany(Inventory::class);
+    }
 }
