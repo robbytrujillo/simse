0e3c54665baa3df5545dec4f3d3d37835f9b322a
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-05 11:31:52 +0700

    achievement model

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Achievement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'achievement_type_id',
+        'date',
+        'description',
+        'achievement_reward_id',
+        'user_id',
+        'image'
+    ];
+
+    public function student() {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function reporter() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function achievmentType() {
+        return $this->belongsTo(AchievementType::class, 'achievement_type_id');
+    }
+    
+    public function achievmentReward() {
+        return $this->belongsTo(AchievementAward::class, 'achievement_reward_id');
+    }
 }
