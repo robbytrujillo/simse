22eb584244cc2467390cc111ac7c735930a1116d
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-05 13:47:19 +0700

    model Kurikulum

--- app/Models/Kurikulum.php
+++ app/Models/Kurikulum.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kurikulum extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'academic_year', 'description'];
+
+    public function syllabuses() {
+        return $this->hasMany(Silabus::class);
+    }
 }
