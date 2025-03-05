ae53073a3b0341b9fefbb7215acd5a8aba13f8b7
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-05 13:52:08 +0700

    option model

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    //
+    use HasFactory;
+
+    protected $filable = [
+        'question_id',
+        'option_text',
+        'is_correct',
+    ];
+
+    public function question() {
+        return $this->belongsTo(Question::class, 'question_id');
+    }
 }
